f24758902b060be73f65a787eacbd9bac1517261
tweimann <t@tweimann.xyz>
2022-08-11 13:01:29 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -12,9 +12,9 @@ bot.onText(/\/scping/, (msg, match) => {
     bot.sendMessage(msg.chat.id, 'Pong! @' + msg.from.username)
 })
 
-bot.onText(/\/scstatus (.+)/, (msg, match) => {
+bot.onText(/\/scecho (.+)/, (msg, match) => {
     if (!settings.chatID.includes(String(msg.chat.id))) {return false}
     if (!settings.adminAccounts.includes(msg.from.username) && !msg.from.is_bot) {return false}
-    bot.sendMessage(msg.chat.id, 'test ' + match[1])
-    console.log(msg)
+    console.log('message recieved: echo')
+    bot.sendMessage(msg.chat.id, 'echo: ' + match[1])
 })
