65e26ff34c6a95c6332b5ea06a57841c6c3af0bb
tweimann <t@tweimann.xyz>
2022-08-12 21:46:16 +0200

    debug is now finished, added msgFilter levels

--- src/bot.js
+++ src/bot.js
@@ -21,26 +21,41 @@ const settings = {
 let bot
 bot = new TelegramBot(auth.telegramAPIKey, { polling: true })
 
+function msgFilter(strength, msg) {
+    if (strength >= 1) {if (msg.from.is_bot) {return false}}
+    if (strength >= 2) {if (!settings.chatID.includes(String(msg.chat.id))) {return false}}
+    if (strength >= 3) {if (!settings.adminAccounts.includes(msg.from.username)) {return false}}
+}
+
 // debug commands
 if (settings.debug) {
     // ping command
     bot.onText(/\/scping/, (msg, match) => {
-        if (!settings.chatID.includes(String(msg.chat.id))) {return false}
-        if (!settings.adminAccounts.includes(msg.from.username) && !msg.from.is_bot) {return false}
+        if (msgFilter(3, msg) == false) {return false}
         log.console('debug', 'message recieved: ping')
         bot.sendMessage(msg.chat.id, 'Pong! @' + msg.from.username)
     })
 
     // echo command
     bot.onText(/\/scecho (.+)/, (msg, match) => {
-        if (!settings.chatID.includes(String(msg.chat.id))) {return false}
-        if (!settings.adminAccounts.includes(msg.from.username) && !msg.from.is_bot) {return false}
+        if (msgFilter(3, msg) == false) {return false}
         log.console('debug', 'message recieved: echo')
         bot.sendMessage(msg.chat.id, 'echo: ' + match[1])
     })
 
-    //bot.sendMessage(settings.chatID[0], 'Debug: Bot started')
-    bot.onText(/\/scmsg (.+)/, (msg) => {
-        log.console('debug', msg)
+    // msg command
+    bot.onText(/\/scmsg (.+)/, (msg, match) => {
+        if (msgFilter(3, msg) == false) {return false}
+        log.console('debug',
+            'message recieved: msg' + '\n' +
+            'User:  ' + msg.from.username + '\n' +
+            'Group: ' + msg.chat.id + '\n' +
+            'Text:  ' + msg.text + '\n' +
+            'Match: ' + match[1]
+        )
     })
+
+    //bot.sendMessage(settings.chatID[0], 'Debug: Bot started')
 }
+
+// static commands
